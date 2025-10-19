fe1502cc7da559e7ddc96ff07f728922041a99fc
Samyak Varia <s08varia@gmail.com>
2025-10-19 19:34:01 +0530

    next.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-/** @type {import('next').NextConfig} */const nextConfig = {  experimental: { typedRoutes: true },};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { typedRoutes: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
